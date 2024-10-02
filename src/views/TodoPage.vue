7d8ed21d4da1b52bbe26ae36e44c1fc82552405a
zoob <zubovalexey2001@gmail.com>
2024-10-02 18:46:18 +0300

    implement list animations

--- src/views/TodoPage.vue
+++ src/views/TodoPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
+import { onMounted, ref } from 'vue'
 import axios from "axios";
+import { gsap } from "gsap";
 
 type ListItem = {
   item_id: number;
@@ -14,7 +15,8 @@ type TodoList = {
 const todoList = ref<TodoList>({
   Items: [],
 });
-const newListItem = ref<String>("");
+const todoItems = ref([]);
+const newListItem = ref<string>("");
 
 const bufferID = ref<number | null>(null);
 const bufferContent = ref<string>();
@@ -22,11 +24,11 @@ const bufferContent = ref<string>();
 const editItem = (id: number, content: string) => {
   bufferID.value = id;
   bufferContent.value = content;
-}
+};
 
 const addListItem = async () => {
   try {
-    const res = await axios.post(`http://${document.location.hostname}:8247/list`, {
+    await axios.post(`http://${document.location.hostname}:8247/list`, {
       user_data: newListItem.value,
     });
     await updateList();
@@ -38,7 +40,7 @@ const addListItem = async () => {
 const updateListItem = async (id: number, content: string | undefined) => {
   try {
     if (content) {
-      const res = await axios.put(`http://${document.location.hostname}:8247/list/${id}`, {
+      await axios.put(`http://${document.location.hostname}:8247/list/${id}`, {
         user_data: content,
       });
     }
@@ -49,7 +51,7 @@ const updateListItem = async (id: number, content: string | undefined) => {
     bufferID.value = null;
     bufferContent.value = "";
   }
-}
+};
 const getList = async () => {
   try {
     const res = await axios.get(`http://${document.location.hostname}:8247/list`);
@@ -57,29 +59,54 @@ const getList = async () => {
   } catch (err) {
     console.log(`There was a problem fetching data: ${err}`);
   }
-}
+};
 const deleteList = async () => {
   try {
-    const res = await axios.delete(`http://${document.location.hostname}:8247/list`);
+    await axios.delete(`http://${document.location.hostname}:8247/list`);
     await updateList();
   } catch (err) {
     console.log(`There was a problem fetching data: ${err}`);
   }
-}
+};
 const deleteListItem = async (id: number) => {
   try {
-    const res = await axios.delete(`http://${document.location.hostname}:8247/list/${id}`);
+    await axios.delete(`http://${document.location.hostname}:8247/list/${id}`);
     await updateList();
   } catch (err) {
     console.log(`There was a problem fetching data: ${err}`);
   }
-}
+};
 const updateList = async () => {
   todoList.value.Items = await getList();
-}
+};
+
+/*const animateListBeforeEnter = (el) => {
+  gsap.set(el, {
+    opacity: 0,
+  })
+};*/
+const animateListEnter = (el, done) => {
+  gsap.from(el, {
+    height: 0,
+    opacity: 0,
+    duration: 0.5,
+    ease: 'power3.inOut',
+    onComplete: done,
+  });
+};
+const animateListLeave = (el, done) => {
+  gsap.to(el, {
+    height: 0,
+    opacity: 0,
+    duration: 0.5,
+    ease: 'power3.inOut',
+    onComplete: done,
+  });
+};
+
 onMounted(() => {
   updateList();
-})
+});
 </script>
 
 <template>
@@ -90,26 +117,29 @@ onMounted(() => {
   </button>
   <div class="todo">
     <h1 class="todo-header">
-      Todo List
+      Zoob List
     </h1>
     <div class="todo-input">
       <input type="text" v-model="newListItem" @keyup.enter="addListItem">
       <button type="submit" @click="addListItem">Add to list</button>
       <button @click="deleteList">Delete</button>
     </div>
-    <div class="todo-list">
-      <div class="todo-list-item" v-for="(item, index) in todoList.Items" :key="index">
-        <div>
-          <span v-if="bufferID !== item.item_id" class="todo-list-item-content" @click="editItem(item.item_id, item.content)">{{ index + 1 }}: {{ item.content }}</span>
-          <input v-else v-model="bufferContent" @blur="updateListItem(item.item_id, bufferContent)" @keyup.enter="updateListItem(item.item_id, bufferContent)" type="text"/>
+      <div class="todo-list">
+        <transition-group tag="div" :css="false" @enter="animateListEnter" @leave="animateListLeave">
+        <div class="todo-list-item" v-for="(item, index) in todoList.Items" :key="item.item_id" ref="todoItems">
+          <div :id="`todo-list-item-content-${item.item_id}`">
+            <span v-if="bufferID !== item.item_id" class="todo-list-item-content"
+                  @click="editItem(item.item_id, item.content)">{{ index + 1 }}: {{ item.content }}</span>
+            <input v-else v-model="bufferContent" @blur="updateListItem(item.item_id, bufferContent)" @keyup.enter="updateListItem(item.item_id, bufferContent)" type="text"/>
+          </div>
+          <div class="todo-list-item-buttons">
+            <button class="todo-list-remove" :id="`list-remove-button-${item.item_id}`"
+                    @click="deleteListItem(item.item_id)">Remove
+            </button>
+          </div>
         </div>
-        <div class="todo-list-item-buttons">
-          <button class="todo-list-remove" :id="`list-remove-button-${item.item_id}`"
-                  @click="deleteListItem(item.item_id)">Remove
-          </button>
-        </div>
+        </transition-group>
       </div>
-    </div>
   </div>
 </template>
 
