a462ec0684d32d81cd3c5be845c5734ef372673c
zoob <zubovalexey2001@gmail.com>
2024-10-11 21:08:39 +0300

    set up eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  {'files': ['**/*.{js,mjs,cjs,ts,vue}']},
+  {'languageOptions': { 'globals': globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  ...pluginVue.configs['flat/essential'],
+  {'files': ['**/*.vue'], 'languageOptions': {'parserOptions': {'parser': tseslint.parser}}},
+  {
+    'rules': {
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'quote-props': ['error', 'always'],
+      'comma-dangle': ['error', 'only-multiline'],
+    },
+  },
 ];
