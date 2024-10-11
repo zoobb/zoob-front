cb460b8f0d29362339edf002c1a4f8dcaaa63dac
zoob <zubovalexey2001@gmail.com>
2024-10-11 21:11:51 +0300

    implement state management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import router from './router'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import router from './router';
+import App from './App.vue';
+import {createPinia} from 'pinia';
 
+const pinia = createPinia();
 const app = createApp(App);
+
 app.use(router);
+app.use(pinia);
 app.mount('#app');
---
+++ src/store/list.ts
@@ -0,0 +1,66 @@
+import {defineStore} from 'pinia';
+import {ref} from 'vue';
+import {Http} from '../api/http.ts';
+
+interface ListItem {
+  item_id: number;
+  content: string;
+}
+
+interface TodoList {
+  Items: ListItem[]
+}
+
+export const useListStore = defineStore('listStore', () => {
+  const client = new Http({
+    'baseURL': `http://${document.location.hostname}:8247`,
+  });
+
+  const list = ref<TodoList>({
+    'Items': [],
+  });
+  const newListItem = ref<string>('');
+
+  const bufferID = ref<number | null>();
+  const bufferContent = ref<string>();
+  const editBuffer = (id: number, content: string) => {
+    bufferID.value = id;
+    bufferContent.value = content;
+  };
+
+  const updateList = async () => {
+    list.value.Items = await client.getList();
+    console.dir(list.value.Items);
+  };
+  const addListItem = async (content: string) => {
+    await client.addListItem(content);
+    await updateList();
+    // console.log('HERE');
+  };
+  const updateListItem = async (id: number, content: string | undefined) => {
+    await client.updateListItem(id, content);
+    await updateList();
+    editBuffer(null, '');
+  };
+  const deleteListItem = async (id: number) => {
+    await client.deleteListItem(id);
+    await updateList();
+  };
+  const deleteList = async () => {
+    await client.deleteList();
+    await updateList();
+  };
+
+  return {
+    list,
+    newListItem,
+    updateList,
+    bufferID,
+    bufferContent,
+    editBuffer,
+    addListItem,
+    updateListItem,
+    deleteListItem,
+    deleteList
+  };
+});
